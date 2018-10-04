81808c562d18acb3da8e2ca3bddc2c45da9c3272
joseph-zhang <zyt5002@126.com>
2018-10-04 23:01:53 +0800

    add new setting

---
+++ setting/desktop-setting.sh
@@ -0,0 +1,8 @@
+# for unity user
+unity-tweak-tool
+
+# for gnome user
+gnome-tweak-tool
+
+# if no tweak-tool, first install them:
+sudo apt-get install <tool-name> 
---
+++ setting/new-apt.sh
@@ -0,0 +1,15 @@
+# In old version of ubuntu, we use apt-get to install and manage packages.
+# New version --> from 16.04, the new apt is used.
+# usage:
+apt install
+apt remove
+apt search
+apt show
+apt update
+apt upgrade
+apt list –installed
+apt list –upgradable
+apt edit-sources
+apt autoremove
+apt purge <package-name>
+# These commands are easier to use.
