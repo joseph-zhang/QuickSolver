5fd20dc86e6253be5dafa9d43f768ba7e797c691
joseph-zhang <zyt5002@126.com>
2018-10-24 13:40:45 +0800

    commit change_kernel

---
+++ setting/change_kernel.sh
@@ -0,0 +1,11 @@
+# change linux kernel version for upgrade or downgrade.
+# first install the target kernel version, suppose the version is x.x.x-x
+
+# install
+sudo apt-get install linux-headers-x.x.x-x-generic linux-image-x.x.x-x-generic
+
+# check the kernel order
+grep 'menuentry' /boot/grub/grub.cfg
+
+# modify grab default in file /etc/default/grub
+GRUB_DEFAULT = the kernel order you want
