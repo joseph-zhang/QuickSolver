e69851778025d7b4cfcbda527818ed6b7168a181
joseph-zhang <zyt5002@126.com>
2018-10-24 16:51:27 +0800

    modified change_kernel

--- setting/change_kernel.sh
+++ setting/change_kernel.sh
@@ -7,7 +7,7 @@ sudo apt-get install linux-headers-x.x.x-x-generic linux-image-x.x.x-x-generic
 # check the kernel order
 grep menuentry /boot/grub/grub.cfg
 
-# modify grab default in file 
+# modify grab default in grub file
 sudo vim /etc/default/grub
 GRUB_DEFAULT = "the kernel order you want e.g. '1>2'"
 
