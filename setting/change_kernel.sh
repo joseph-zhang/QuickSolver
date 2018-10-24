a7c8aedc081f7bdf44895eb3789b257ed23bb3ee
joseph-zhang <zyt5002@126.com>
2018-10-24 16:50:06 +0800

    modified change_kernel

--- setting/change_kernel.sh
+++ setting/change_kernel.sh
@@ -5,13 +5,22 @@
 sudo apt-get install linux-headers-x.x.x-x-generic linux-image-x.x.x-x-generic
 
 # check the kernel order
-grep 'menuentry' /boot/grub/grub.cfg
+grep menuentry /boot/grub/grub.cfg
 
-# modify grab default in file /etc/default/grub
-GRUB_DEFAULT = the kernel order you want
+# modify grab default in file 
+sudo vim /etc/default/grub
+GRUB_DEFAULT = "the kernel order you want e.g. '1>2'"
 
 # upgrade
 sudo update-grub
 
 # then reboot, check the kernel version
 uname -a
+
+#----------------------------------------------------------------
+# Tips
+#----------------------------------------------------------------
+# If the setting of GRUB_DEFAULT is bad, you may not start
+# the system directly, but start with some applications such
+# as memory test. This is not an error, just press and hold
+# "Shift" key, the grub will be shown on screen.
