ee53366fdb3c95fa86d8cfebf85980cc5ee6b4c6
joseph-zhang <zyt5002@126.com>
2018-10-31 23:33:30 +0800

    commit set-env-v2

---
+++ setting/set-env-v2.sh
@@ -0,0 +1,13 @@
+# 2018/10/31
+# Another development environment setting memo.
+# System OS : Manjaro
+# DE : no DE :)
+# WM : i3
+# CPU : Intel Core i7-7700HQ
+# GPU : Geforce GTX1060
+#------------------------------------------------------
+# fonts:
+#     system: sarasa-mono-sc-regular
+#     editor: iosevka-cc-slab-extralight
+#     CJK for latex: Source Han Serif SC
+#------------------------------------------------------
